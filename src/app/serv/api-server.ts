9e14ce409b3d2b9cc21bcf2206f9f6f73a53f42f
deakin-yy <744203117@qq.com>
2017-03-07 15:56:35 +0800

    Observable

--- src/app/serv/api-server.ts
+++ src/app/serv/api-server.ts
@@ -3,7 +3,7 @@
  */
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
